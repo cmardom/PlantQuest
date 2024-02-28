0ece10278658e7db9d59ce4a97439328ec858f72
Carmen <cmardom599@g.educaand.es>
2024-02-28 17:06:21 +0100

    init

---
+++ src/main/java/org/iesvdm/proyecto_plantquest/ProyectoPlantQuestApplication.java
@@ -0,0 +1,13 @@
+package org.iesvdm.proyecto_plantquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPlantQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoPlantQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iesvdm/proyecto_plantquest/ProyectoPlantQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesvdm.proyecto_plantquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPlantQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
