87e55a7ddc25e48428734d32ec817ffef80fd256
Carmen <95345941+muffinsita@users.noreply.github.com>
2024-07-16 12:24:54 +0200

    bootstrap / carrousel not working

--- src/main/frontend/app-plantquest/src/app/app.component.ts
+++ src/main/frontend/app-plantquest/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavComponent} from "./nav/nav.component";
+import {HeroComponent} from "./hero/hero.component";
+import {CarrouselComponent} from "./carrousel/carrousel.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, HeroComponent, CarrouselComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/main/frontend/app-plantquest/src/app/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: '/dist/css/bootstrap.css',
+})
+export class NavComponent {
+
+}
